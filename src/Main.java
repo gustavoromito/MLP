e46ac5147736dd63c194725d732dae4fd4fd6045
Gustavo Romito <guromito@gmail.com>
2017-05-01 18:28:01 -0300

    Working in progress

--- src/Main.java
+++ src/Main.java
@@ -13,38 +13,31 @@ import java.util.Random;
 public class Main {
 
     /** Parametros da Rede Neural */
-    public static double TAXA_APRENDIZADO = 0.1;
-    public static int NUMERO_EPOCAS = 10;
-
-    // taxa de aprendizado
-    // numero de epocas
-    // condição de parada:
-    //  - por epoca
-    //  - por erro
-    //  - taxa de aprendizado
-    // Estagio FeedForward
-    // Estagio Retropropagaçao do erro
-    // Estagio Atualização dos Pesos
-    // Bias fixo
-    // Matriz Camada de Entrada
-    // Matriz de Pesos Entrada para Camada Interna
-    // Matriz de Pesos Camada Interna para Saída
-    // Uma função para função de ativação
-    // uma Camada escondida
-    // uma Camada de Saída
+    public static double TAXA_APRENDIZADO = 0.5;
+    public static int NUMERO_EPOCAS = 10000;
+    public static int NUMERO_NEURONIOS_CAMADA_SAIDA = 3;
 
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
+    public static double[] insertBiasToHog(float[] input) {
+        if (input == null) {
+            return null; // Or throw an exception - your choice
+        }
+        double[] output = new double[input.length + 1];
+        /* First element is the Biar */
+        output[0] = 1;
+
+        for (int i = 1; i <= input.length; i++) {
+            output[i] = input[i - 1];
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
 
-        float[] entrada = hogDescriptor();
         /* We need to insert bias equals 1 here in first element */
-
-        double[] camadaEntrada = {1, 4, 6, 8};
-
-        double[][] pesos = randomPesos(camadaEntrada);
 
         // Valores da camada de entrada => [b1, x1, x2, x3]
 
@@ -54,62 +47,111 @@ public class Main {
         //	| v02, v12, v22, v32 |
         //	| v03, v13, v23, v33 |
         //
-        double[][] pcamadaEntrada = { {2, 2, 2, 2}, {-4, -4, -4, -4}, {6, 6, 6, 6} };
-        double[][] pcamadaEscondida = { {3,3,3,3}, {5,5,5,5}, {7,7,7,7} };
+//        double[][] pcamadaEntrada = { {2, 2, 2, 2}, {-4, -4, -4, -4}, {6, 6, 6, 6} };
+//        double[][] pcamadaEscondida = { {3,3,3,3}, {5,5,5,5}, {7,7,7,7} };
 
-        double[] camadaEscondida = calcularSomatorio(camadaEntrada, pcamadaEntrada);
+        String[] images = new String[10];
+        for (int i = 0; i < images.length; i++) {
+            images[i] = "train_53_0100" + i + ".png";
+        }
 
         /* Camada de Saída possui index 0 com bias fixo. Consumir a partir do index 1. */
-        double[] camadaSaida = calcularSomatorio(camadaEscondida, pcamadaEscondida);
+        for (int i = 0; i < images.length; i++) {
+            System.out.println("RODANDO IMAGEM: " + images[i]);
 
-        int[] valoresEsperadosCamadaSaida = { 1, 0, 0 };
-        testCalcularErro(valoresEsperadosCamadaSaida, camadaSaida, pcamadaEscondida, camadaEscondida);
+            float[] entrada = hogDescriptor(images[i]);
+
+            int[] valoresEsperados = valoresEsperadosForFileName(images[i]);
+            double[] camadaEntrada = insertBiasToHog(entrada);
+            double[][] pcamadaEntrada = randomPesos(camadaEntrada);
+
+            double[] camadaEscondida = calcularSomatorio(camadaEntrada, pcamadaEntrada, false);
+            double[][] pcamadaEscondida = randomPesos(camadaEscondida);
+
+            double[] camadaSaida = new double[NUMERO_NEURONIOS_CAMADA_SAIDA];
+
+            int j = 0;
+            while (TAXA_APRENDIZADO < 1 || j < NUMERO_EPOCAS) {
+
+                //FEED FORWARD
+                camadaEscondida = calcularSomatorio(camadaEntrada, pcamadaEntrada, false);
+                camadaSaida = calcularSomatorio(camadaEscondida, pcamadaEscondida, true);
+                // END FEED FORWARD
+
+                //BACK PROPAGATION
+                double[] erroSaida = calcularErro(valoresEsperados, camadaSaida);
+                double[][] deltaPesosCamadaEscondida = deltaPesos(erroSaida, camadaEscondida, camadaSaida.length);
+                double[] deltaBiasCamadaEscondida = deltaPesosBias(erroSaida);
+
+                double[] erroEntrada = calcularErroFinal(erroSaida, camadaEscondida, pcamadaEscondida);
+                double[][] deltaPesosCamadaEntrada = deltaPesos(erroEntrada, camadaEntrada, camadaEscondida.length);
+                double[] deltaBiasCamadaEntrada = deltaPesosBias(erroEntrada);
+
+                double[][] novosPesosCamadaEscondida = atualizaPesos(pcamadaEscondida, deltaPesosCamadaEscondida, deltaBiasCamadaEscondida);
+                double[][] novosPesosCamadaEntrada = atualizaPesos(pcamadaEntrada, deltaPesosCamadaEntrada, deltaBiasCamadaEntrada);
+
+                pcamadaEntrada = novosPesosCamadaEntrada;
+                pcamadaEscondida = novosPesosCamadaEscondida;
+                // END BACK PROPAGATION
+
+                TAXA_APRENDIZADO += 0.001;
+                j++;
+            }
+
+            System.out.println("TERMINOU IMAGEM: " + images[i]);
+        }
+        System.out.print(true);
     }
 
-    public static void testCalcularErro(int[] valoresEsperados, double[] camadaSaida, double[][] pcamadaEscondida, double[] camadaEscondida ) {
-        /* TODO: Precisamos mudar esse metodo calcular erro pra ele receber
-                    um array de valores esperados e não apenas 1 valor esperado */
-        double[] erro = calcularErro(valoresEsperados[0], camadaSaida);
-        double[] erroFinal = calcularErroFinal(erro, camadaEscondida, pcamadaEscondida);
-
-        double[][] deltaPesos = deltaPesos(erroFinal, camadaEscondida);
-        double[] deltaBias = deltaPesosBias(erroFinal);
-        double[][] novosPesosCamadaEscondida = atualizaPesos(pcamadaEscondida, deltaPesos, deltaBias);
+    public static int[] valoresEsperadosForFileName(String filename) {
+        /*
+           train_5a_ = Z
+           train_53_ = S
+           train_58_ = X
+        */
+        if (filename.contains("train_5a_")) {
+            return new int[]{ 1 , 0 , 0 };
+        } else if (filename.contains("train_53_")) {
+            return new int[]{ 0 , 1 , 0 };
+        } else {
+            return new int[]{ 0 , 0 , 1 };
+        }
     }
 
-    public static double[] calcularErro(int valorEsperado, double[] camadaSaida) {
-        double[] erro = new double[camadaSaida.length - 1];
-        for(int k = 0; k < camadaSaida.length - 1; k++){
-            erro[k] = (valorEsperado - camadaSaida[k]) * derivadaFuncaoExponencial(camadaSaida[k]);
+    public static double[] calcularErro(int[] valorEsperado, double[] camadaSaida) {
+        double[] erro = new double[camadaSaida.length];
+        for(int k = 0; k < camadaSaida.length; k++){
+            erro[k] = (valorEsperado[k] - camadaSaida[k]) * derivadaFuncaoExponencial(camadaSaida[k]);
         }
         return erro;
     }
     
     public static double[] calcularErroFinal(double[] erro, double[] camadaEscondida, double[][] pcamadaSaida) {
-        double[] aux = new double[3];
-        double[] erro2 = new double[3];
+        double[] aux = new double[pcamadaSaida.length];
         // Parte da somatória para achar os valores de entrada da camada escondida
         for(int i = 0; i < pcamadaSaida.length; i++) {
-            int somatoria = 0;
-            for(int j = 0; j < pcamadaSaida[i].length; j++) {
+            double somatoria = 0;
+            for(int j = 0; j < erro.length; j++) {
                 somatoria += erro[j] * pcamadaSaida[i][j];
             }
             aux[i] = somatoria;
         }
-        for(int k = 0; k < erro2.length; k++){
-            erro2[k] = aux[k] * derivadaFuncaoExponencial(camadaEscondida[k]);
+
+        double[] erroFinal = new double[pcamadaSaida.length];
+        for(int k = 0; k < erroFinal.length; k++){
+            erroFinal[k] = aux[k] * derivadaFuncaoExponencial(camadaEscondida[k]);
         }
 
-        return erro2;
+        return erroFinal;
     }
     
     // CAMADAX DEVE SER OU A CAMADA ESCONDIDA OU A CAMADA DE ENTRADA,
     // DEPENDE DE QUAL DELTA ESTAMOS QUERENDO, OU EH O DELTA W OU
     // O DELTA V
-    public static double[][] deltaPesos(double[] erro, double[] camadaX){
-        double[][] deltapesos = new double[camadaX.length][camadaX.length];
-        for(int i = 0; i < camadaX.length - 1; i++){
-            for(int j = 0; j < camadaX.length - 1; j++){
+    public static double[][] deltaPesos(double[] erro, double[] camadaX, int nextCamadaLength){
+        double[][] deltapesos = new double[camadaX.length][nextCamadaLength];
+        for(int i = 0; i < deltapesos.length; i++) {
+            for(int j = 1; j < erro.length; j++){
                 deltapesos[i][j] = TAXA_APRENDIZADO * erro[j] * camadaX[i];
             }
         }
@@ -119,7 +161,7 @@ public class Main {
     // ARRAY ERRO PODE SER O ERRO DA CAMADA ESCONDIDA OU
     // DA CAMADA DE ENTRADA
     public static double[] deltaPesosBias(double[] erro){
-        double[] dpbias = new double[3];
+        double[] dpbias = new double[erro.length];
         for(int i = 0; i < dpbias.length; i++){
             dpbias[i] = TAXA_APRENDIZADO * erro[i];
         }
@@ -128,14 +170,14 @@ public class Main {
     
     public static double[][] atualizaPesos(double[][] pcamadaX, double[][] deltapeso, double[] dpbias){
         double[][] novosPesos = new double[pcamadaX.length][pcamadaX[0].length];
-        for(int j = 1; j < pcamadaX.length; j++){
-            for(int k = 0; k < pcamadaX[j].length; k++){
+        for(int j = 0; j < pcamadaX.length; j++){
+            for(int k = 1; k < deltapeso[j].length; k++){
                 novosPesos[j][k] = pcamadaX[j][k] + deltapeso[j][k];
             }
         }
         
         for(int i = 0; i < dpbias.length; i++){
-            novosPesos[i][i] = pcamadaX[i][i] + dpbias[i];
+            novosPesos[i][0] = pcamadaX[i][0] + dpbias[i];
         }
 
         return novosPesos;
@@ -146,8 +188,8 @@ public class Main {
 
         double[][] pesos = new double[camada.length - 1][camada.length];
 
-        for (int i = 0; i < camada.length - 1; i++) {
-            for (int j = 0; j < camada.length; j++) {
+        for (int i = 0; i < pesos.length; i++) {
+            for (int j = 0; j < pesos[i].length; j++) {
                 pesos[i][j] = r.nextDouble();
             }
         }
@@ -155,24 +197,24 @@ public class Main {
     }
 
     // Retorna um array com os valores dos somatorios para a camada seguinte
-    public static double[] calcularSomatorio(double[] camadaX, double[][] camadaPesos) {
+    public static double[] calcularSomatorio(double[] camadaX, double[][] camadaPesos, boolean isCamadaSaida) {
         // Variavel para guardar o valor da somatoria da entrada do neuronio * peso do neuronio
 
-        double[] novaCamada = new double[camadaX.length + 1];
-        // Setando o primeiro valor no vetor, que será o bias
-        novaCamada[0] = 1;
+        int length = isCamadaSaida ? NUMERO_NEURONIOS_CAMADA_SAIDA : camadaX.length;
+        double[] novaCamada = new double[length];
 
-        // Contador que auxiliará para setar os valores no array da camada escondida
-        int aux = 1;
+        // Setando o primeiro valor no vetor, que será o bias
+        if (!isCamadaSaida) novaCamada[0] = 1;
 
         // Parte da somatória para achar os valores de entrada da camada escondida
-        for(int i = 0; i < camadaPesos.length; i++) {
-            int somatoria = 0;
-            for(int j = 0; j < camadaPesos[i].length; j++) {
-                somatoria += camadaX[j] * camadaPesos[i][j];
+        int initial = (isCamadaSaida) ? 0 : 1;
+        for(int i = initial; i < novaCamada.length; i++) {
+            double somatoria = 0;
+            int index = Math.max(i - 1, 0);
+            for(int j = 0; j < camadaPesos[index].length; j++) {
+                somatoria += camadaX[j] * camadaPesos[index][j];
             }
-            novaCamada[aux] = funcaoExponencial(somatoria);
-            aux++;
+            novaCamada[i] = funcaoExponencial(somatoria);
         }
 
         return novaCamada;
@@ -180,19 +222,19 @@ public class Main {
 
 
     /* FUNCAO DE ATIVACAO */
-    public static double funcaoExponencial(int sum) {
-        return 1 / (1 + Math.exp(-sum));
+    public static double funcaoExponencial(double sum) {
+        return 1.0 / (1.0 + Math.exp(-sum));
     }
 
     // Fx = Resultado da Funcao Exponencial
     public static double derivadaFuncaoExponencial(double fx) {
-        return fx * (1 - fx);
+        return fx * (1.0 - fx);
     }
 
-    public static float[] hogDescriptor() {
+    public static float[] hogDescriptor(String filename) {
 //        carrega uma img, o parametro é o caminho para a imagem
-        String projectAbsolutePath = "/Users/gustavoromito/Companies/USP Faculdade/IA/Projeto/out/production/Projeto/";
-        Mat img = Highgui.imread(projectAbsolutePath + "train_5a_00000.png");
+        String projectAbsolutePath = "/Users/gustavoromito/Companies/USP Faculdade/IA/dataset1/testes/";
+        Mat img = Highgui.imread(projectAbsolutePath + filename);
 
         //HOG
         HOGDescriptor hog = new HOGDescriptor(
@@ -213,10 +255,10 @@ public class Main {
                 //nbins: sets the number of bins in the histogram of gradients. The authors of the HOG paper had
                 //recommended a value of 9 to capture gradients between 0 and 180 degrees in 20 degrees increments.
 
-                new Size(16, 16), //winSize
-                new Size(16, 16), //blocksize
-                new Size(16, 16), //blockStride
-                new Size(16, 16), //cellSize
+                new Size(64, 64), //winSize
+                new Size(64, 64), //blocksize
+                new Size(64, 64), //blockStride
+                new Size(64, 64), //cellSize
                 9 //nbins
         );
 
@@ -229,12 +271,6 @@ public class Main {
         floats.channels();
         floats.depth();
         float a[] = floats.toArray();
-
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-            if (i % 6 == 1)
-                System.out.print("END OF LINE\n");
-        }
 
         return a;
     }
